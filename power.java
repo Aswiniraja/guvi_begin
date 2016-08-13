3ae4765e07bfb2eeb422b6f87737cbf413522e5b
Aswiniraja <aswinirajaeee@gmail.com>
2016-08-13 17:37:11 +0530

    Create power.java

---
+++ power.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class power_11
+{
+
+	public static void main(String[] args) 
+	{
+		Scanner sc=new Scanner(System.in);
+
+		double c=Math.pow(sc.nextInt(), sc.nextInt());
+		
+		System.out.println(c);
+		
+		
+		sc.close();
+
+	}
+
+}
