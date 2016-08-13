31f4a06cbaa09b88ed761e2aa8b468a29c6cd199
Aswiniraja <aswinirajaeee@gmail.com>
2016-08-13 16:32:56 +0530

    Create leapyr.java

---
+++ leapyr.java
@@ -0,0 +1,21 @@
+
+import java.util.Scanner;
+
+public class leapyr_6 
+{
+	public static void main(String[] args) 
+	{
+		Scanner sc=new Scanner(System.in);
+		
+		int a=sc.nextInt();
+		
+		if(a%4==0 && a%100!=0)
+			System.out.println("leap year");
+		else
+			System.out.println("not a leap year");
+
+		sc.close();
+	}
+
+
+}
