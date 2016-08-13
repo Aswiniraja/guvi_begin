d2264c8765ce09553b70c5a5fd03939dd9a234f6
Aswiniraja <aswinirajaeee@gmail.com>
2016-08-13 17:35:43 +0530

    Create intcount.java

---
+++ intcount.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class intcount_10 
+{
+	public static void main(String[] args)
+	{
+
+		Scanner sc=new Scanner(System.in);
+
+		int c=String.valueOf(sc.nextInt()).length();
+		System.out.println(c);
+		
+		
+		sc.close();
+		
+	
+	}
+
+}
